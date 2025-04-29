50b6846a084b3a44e7cb93bc419bc769b63baeda
ILUMINX89 <93902067+ILUMINX89@users.noreply.github.com>
2025-04-28 21:51:11 -0500

    Update prueba.js

---
+++ prueba.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let number2 = 2;
+    document.getElementById("prueba").addEventListener("click", function() {
+        alert("Hola mundo");
+    });
+});
