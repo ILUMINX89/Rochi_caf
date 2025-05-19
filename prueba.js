bfb2853ea8ee43eea420cfa2c1bebd83e10c7664
Esneider2406 <esneidervelasco7@gmail.com>
2025-05-19 18:25:43 -0500

    a

---
+++ prueba.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let number2 = "Hola mundo";
+    document.getElementById("prueba").addEventListener("click", function() {
+        alert(number2);
+    });
+});
